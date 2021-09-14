2145ff83e5a299df75484d335e6d64aab7a0ebd7
tfauvelle@gmail.com <tfauvelle@gmail.com>
2021-09-14 18:52:23 -0400

    made card fields private.

--- src/com/company/card/Card.java
+++ src/com/company/card/Card.java
@@ -1,15 +1,26 @@
 package com.company.card;
 
 public class Card {
-    public final String suit;
-    public final String faceValue;
+    private final String suit;
+    private final int faceValue;
 
-    public Card(String suit, String faceValue) {
+    public Card(String suit, int faceValue) {
         this.suit = suit;
-        this.faceValue =faceValue;
+        this.faceValue = faceValue;
     }
 
+    public int getFaceValue() {
+        return faceValue;
+    }
     public String toString() {
         return faceValue + " " + suit ;
     }
+
+    public String display() {
+        String outPut = " ";
+        switch (faceValue) {
+            case
+        }
+    }
+
 }
