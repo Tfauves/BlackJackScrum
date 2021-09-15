e0374531e9f2a7c80e070f45982e7398b031b6b5
tfauvelle@gmail.com <tfauvelle@gmail.com>
2021-09-15 13:45:39 -0400

    override deal method in cheat deck.

--- src/com/company/card/CheatersDeck.java
+++ src/com/company/card/CheatersDeck.java
@@ -1,5 +1,6 @@
 package com.company.card;
 
+import com.company.Console;
 import com.company.Deck;
 
 import java.util.Arrays;
@@ -15,27 +16,27 @@ public class CheatersDeck implements Deck {
 
     public void shuffle() {}
 
-
-    public Card deal() {
-
-        System.out.println(Arrays.toString(SUITS));
-        System.out.println("Which suit? (1-4)");
-        try {
-            int suitIndex = Integer.parseInt(scanner.nextLine()) - 1;
-            System.out.println(Arrays.toString(FACE_VALUE));
-            System.out.println("Which value? (1-13)");
-            int valueIndex = Integer.parseInt(scanner.nextLine()) - 1;
-            chosenCard = new Card(SUITS[suitIndex], FACE_VALUE[valueIndex]);
-        } catch (Exception e) {
-            System.out.println("Invalid choice! SELECT again: ");
-        }
-        return chosenCard;
-    }
+//
+//    public Card deal() {
+//
+//        System.out.println(Arrays.toString(SUITS));
+//        System.out.println("Which suit? (1-4)");
+//        try {
+//            int suitIndex = Integer.parseInt(scanner.nextLine()) - 1;
+//            System.out.println(Arrays.toString(FACE_VALUE));
+//            System.out.println("Which value? (1-13)");
+//            int valueIndex = Integer.parseInt(scanner.nextLine()) - 1;
+//            chosenCard = new Card(SUITS[suitIndex], FACE_VALUE[valueIndex]);
+//        } catch (Exception e) {
+//            System.out.println("Invalid choice! SELECT again: ");
+//        }
+//        return chosenCard;
+//    }
 
     @Override
-    public Card draw() {
-        int value = Console.getInt(1, 13, "enter number 1-13", "invalid entry");
+    public Card deal() {
+        int faceValue = Console.getInt(1, 13, "enter number 1-13", "invalid entry");
         int suit = Console.getInt(1, 4, "1. \u2664 | 2. \u2665 | 3. \u2666 | 4. \u2667", "Invalid entry");
-        return new Card(value, SUITS[suit - 1]);
+        return new Card(SUITS[suit - 1], faceValue);
     }
 }
