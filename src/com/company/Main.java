af7e564a7001aefe1f844226252b2c8fa351ea35
tfauvelle@gmail.com <tfauvelle@gmail.com>
2021-09-21 20:30:59 -0400

    tested, can deal a hand.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,28 @@
 package com.company;
 
+import com.company.cardGame.blackJack.Hand;
 import com.company.cardGame.deck.CheatersDeck;
 import com.company.cardGame.deck.Deck;
+import com.company.cardGame.deck.StandardDeck;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Deck cardDeck = new CheatersDeck();
+        Deck cardDeck = new StandardDeck();
         cardDeck.shuffle();
-        System.out.println(cardDeck.deal().display());
-        System.out.println(cardDeck.deal().display());
-        System.out.println(cardDeck.deal().display());
-        System.out.println(cardDeck.deal().display());
-        System.out.println(cardDeck.deal().display());
-        System.out.println(cardDeck.deal().display());
+//        System.out.println(cardDeck.deal().display());
+//        System.out.println(cardDeck.deal().display());
+//        System.out.println(cardDeck.deal().display());
+//        System.out.println(cardDeck.deal().display());
+//        System.out.println(cardDeck.deal().display());
+//        System.out.println(cardDeck.deal().display());
+        Hand myhand = new Hand();
+        myhand.addCard(cardDeck.deal());
+        myhand.addCard(cardDeck.deal());
+        System.out.println(myhand.displayHand());
+        myhand.addCard(cardDeck.deal());
+        myhand.addCard(cardDeck.deal());
+        System.out.println(myhand.displayHand());
     }
 }
--- src/com/company/cardGame/blackJack/Hand.java
+++ src/com/company/cardGame/blackJack/Hand.java
@@ -1,6 +1,7 @@
 package com.company.cardGame.blackJack;
 
 import com.company.cardGame.deck.Card;
+import com.company.cardGame.deck.Deck;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +11,23 @@ public class Hand {
     private int bet = 0;
 //    private Player player;
 
+    public void addCard(Card card) {
+        cards.add(card);
+    }
 
+//    public void addCard(Deck deck) {
+//        addCard(deck.deal());
+//    }
+
+    public String displayHand() {
+        String outPut = "";
+        for (Card card : cards) {
+            outPut += card.display() + " ";
+        }
+
+        return outPut.toString().trim();
+
+    }
 
 
 }
