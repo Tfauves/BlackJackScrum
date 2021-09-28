6df03c6e02685bec932fa661b210689eebc3530a
tfauvelle@gmail.com <tfauvelle@gmail.com>
2021-09-27 20:52:13 -0400

    display table and determine winner.

--- src/com/company/cardGame/blackJack/Table.java
+++ src/com/company/cardGame/blackJack/Table.java
@@ -6,7 +6,7 @@ import com.company.cardGame.deck.Deck;
 import com.company.cardGame.deck.StandardDeck;
 
 public class Table {
-    Hand playerHand = new Hand(new Player("player"));
+    Hand player = new Hand(new Player("player"));
     Hand dealer = new Hand(new Dealer());
     Deck deck;
 
@@ -17,7 +17,31 @@ public class Table {
     }
 
     private void deal() {
-        deck.draw();
+       for (int count = 0; count < 2; count++) {
+           player.addCard(deck.draw());
+           dealer.addCard(deck.draw());
+       }
+    }
+
+    private void displayTable() {
+        StringBuilder outPut = new StringBuilder();
+        outPut.append("Dealer: ").append(dealer.displayHand()).append("\n");
+        outPut.append("Player: ").append(player.displayHand()).append("\n");
+        System.out.println(outPut);
+
+    }
+
+    private void determineWinner() {
+        if (player.getValue() > dealer.getValue()) {
+            System.out.println("Player Wins");
+            return;
+        }
+        if (player.getValue() == dealer.getValue()) {
+            System.out.println("Push");
+            return;
+        }
+        System.out.println("Dealer Wins");
+
     }
 
 
