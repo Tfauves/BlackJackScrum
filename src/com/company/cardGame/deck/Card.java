573bba52a995b0e59cc9c29b93c3e953f3c6242b
tfauvelle@gmail.com <tfauvelle@gmail.com>
2021-10-04 19:22:16 -0400

    cards can be facedown

--- src/com/company/cardGame/deck/Card.java
+++ src/com/company/cardGame/deck/Card.java
@@ -3,6 +3,7 @@ package com.company.cardGame.deck;
 public class Card {
     private final String suit;
     private final int rank;
+    private boolean isFaceDown = true;
 
     public Card(String suit, int rank) {
         this.suit = suit;
@@ -19,6 +20,9 @@ public class Card {
 
     public String display() {
         String outPut = "";
+        if (isFaceDown) {
+            return "<*>";
+        }
         switch (rank) {
             case 1 -> outPut = "AC";
             case 11 -> outPut = "JK";
@@ -28,6 +32,10 @@ public class Card {
             //            case 0 -> output = "JR"; //joker
         };
         return outPut + " " + suit;
+    }
+
+    public void flip() {
+        isFaceDown = !isFaceDown;
     }
 
 }
